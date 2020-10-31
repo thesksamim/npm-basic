2a80d028353321ec0b8c6f8e866ed965016f3ce2
Sekh Samim <68887683+thesksamim@users.noreply.github.com>
2020-10-31 12:03:27 +0530

    Add files via upload

---
+++ 01_npm.js
@@ -0,0 +1,15 @@
+// write "npm init" for creatiing package.json 
+// when you install any package using "npm install anything" node_module and package-lock.json is created .
+// if you want to install any spcific version , write "npm install anything@version"
+// if node_module is deleted by any chance , write "npm install" in the terminal 
+
+// if you want to use any packege only for devolpment purpose not for production purpose then use 
+//"npm install anything --save-dev " . example nodemon
+
+// to install any package globally use "npm install anything --global"
+
+// If you want to start terminal automatically every time you change something and save it or change the file 
+// use nodemon 
+// To start nodemon use "nodemon filename" in the terminal after installing it .
+
+console.log("this is npm 1 tutorial");
